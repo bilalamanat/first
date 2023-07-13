ee342505f9a461d66071f4319890c28925872731
Bilal Amanat <bilalamanat17@gmail.com>
2023-07-14 03:18:43 +0500

    React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Navbar from "./Components/Nav/Navbar";
+
+
 
 function App() {
+  const [wives, setWives] = useState(0);
+
+  const [kids, setKids] = useState(0);
+
+  const addWife = () => {
+    setWives((prevWives) => prevWives + 1);
+    setKids((prevKids) => prevKids + 3);
+  };
+
+  const minWife = () => {
+    if (wives > 0) {
+      setWives(wives - 1);
+    }
+  };
+
+  const resetValue = () => {
+    setWives(0);
+    setKids(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Navbar />
+      <h1>{wives === 1 ? "Wife": "Number of Wives"}: {wives}</h1>
+      <h1>Number of kids: {kids}</h1>
+      <button onClick={minWife}>-</button>
+
+      <button onClick={addWife}>+</button>
+      <button onClick={resetValue}>Reset</button>
     </div>
   );
 }
---
+++ src/Components/Nav/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <a className="navbar-brand" href="#">Logo</a>
+        <ul className="navbar-menu">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
