32ae17bc29faec2e6435f8c2f0cdae83498e6eeb
Bilal Amanat Muhammadi <136390430+bilalamanat@users.noreply.github.com>
2023-08-02 02:45:18 +0500

    Scripit Add

--- src/Components/BarberCode/index.jsx
+++ src/Components/BarberCode/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+
 import Banner from '../futhertukray/Banner'
 import Services from '../futhertukray/Services'
 import Footer from '../futhertukray/Footer'
@@ -6,6 +7,30 @@ import Feature from '../futhertukray/Feature'
 import Testimonail from '../futhertukray/Testimonail'
 
 function BarberShop() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.innerHTML = `
+    var manubtn = document.getElementById("manubtn")
+    var sideNav = document.getElementById("sideNav")
+    var manu = document.getElementById("MANU")
+    sideNav.style.right = "-250px"
+    manubtn.onclick = function () {
+        if (sideNav.style.right == "-250px") {
+            sideNav.style.right = "0";
+            manu.src = "img/x-solid.svg";
+        }
+        else {
+            sideNav.style.right = "-250px";
+            manu.src = "img/menu-icon.svg";
+        }
+    }
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        speed: 1000,
+        speedAsDuration: true
+    });
+    `;
+    document.body.appendChild(script);
+  }, []);
   return (
     <div>
     <title>Bilal Shop Website Design</title>
